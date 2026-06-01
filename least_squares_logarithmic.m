8d730a346b27e49e3c569bde0490bc85c34d10d8
aryankumar2005 <iparyan2005@gmail.com>
2026-06-01 01:08:46 +0100

    least_squares_logarithmic.m

--- least_squares_logarithmic.m
+++ least_squares_logarithmic.m
@@ -14,11 +14,9 @@ sumy = sum(y);
 sumu2 = sum(u.^2);
 sumuy = sum(u.*y);
 
-% Minimising sum of squared residuals requires dS/db1 = 0 and dS/db0 = 0.
-% This gives the 2x2 normal equations below.
+
 A = [sumu2, sumu;
      sumu, n    ];
-
 b = [sumuy;
      sumy ];
 
